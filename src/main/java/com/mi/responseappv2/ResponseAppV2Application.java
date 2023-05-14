90948bf96b8857ee3b1b79ca6699cc5358076ce3
Anton_Mishchuk <amishchuk@vertafore.com>
2023-05-14 11:41:38 +0300

    init commit

---
+++ src/main/java/com/mi/responseappv2/ResponseAppV2Application.java
@@ -0,0 +1,13 @@
+package com.mi.responseappv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponseAppV2Application.class, args);
+	}
+
+}
