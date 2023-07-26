bcb371bb308b2ce43a725d8904433b5d6447d4a8
Anton_Mishchuk <amishchuk@vertafore.com>
2023-07-26 13:26:52 +0300

    add one more gas station

--- data.sql
+++ data.sql
@@ -7,3 +7,5 @@ INSERT INTO public.gas_station(id, address, chat_id)
 values ('7c4353f6', 'м.Устилуг, вул. Володимирська', '180845064');
 INSERT INTO public.gas_station(id, address, chat_id)
 values ('845eac3e', 'м. Ковель, вул. Сагайдачного', '180845064');
+INSERT INTO public.gas_station(id, address, chat_id)
+values ('ff0128fa', 'м.Нововолинськ вул.Шахтарська', '180845064');
